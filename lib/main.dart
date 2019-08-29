f594e1fca93d87351f9ed85f8db055ae85584212
ranabhawana <ranabhawana012@gmail.com>
2019-08-29 11:15:33 +0545

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-void main()=> runApp(new MyApp());
+//import 'package:flutter/rendering.dart';
+import 'x.dart';
+import 'login.dart';
+
+
+void main()=> runApp(new MaterialApp(
+  home: new LoginPage(),
+  routes: <String, WidgetBuilder>{
+    "/": (BuildContext context) => new LoginPage(),
+    "/SecondPage": (BuildContext context) => new SecondPage()
+  }
+));
+
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -21,89 +32,8 @@ class MyApp extends StatelessWidget{
         
         }
         
-        class LoginPageState extends State<LoginPage> {
+        
 
-          @override
-          Widget build(BuildContext context) {
-            return new Scaffold(
-            backgroundColor: Colors.greenAccent,
-            body:new Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                new Image(
-                  image: new AssetImage ("assets/background.jpg"),
-                  fit: BoxFit.cover,
-                  color: Colors.black87,
-                  colorBlendMode: BlendMode.darken,
-                ),
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[ 
-                    new Image(
-                      image: new AssetImage("assets/logo.jpg"),
-                      height: 100.0,
-                      width: 80.0,
-                    ),
-                    new Form(
-                      child: new Theme(
-                        data: new ThemeData(
-                        brightness: Brightness.dark,
-                        primarySwatch: Colors.teal
-                        ),
-                          child: new Column(
-                          children: <Widget>[
-                            new TextFormField(
-                              decoration: new InputDecoration(
-                                hintText: "Enter Email",
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                            ),
-                            new TextFormField(
-                              decoration: new InputDecoration(
-                                hintText: "Enter Password",
-                              ),
-                              keyboardType: TextInputType.text,
-                              obscureText: true,  //obsecure text by not showing it
-                            ),
-                            new Column(
 
-                              children: <Widget>[
-                                RaisedButton(
-                                child: Text("Login"),
-                                 onPressed: sendToDatabase,
-                                  color: Colors.red,
-                                  textColor: Colors.white,
-                                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                  splashColor: Colors.grey,
-                                ),
-                                ]
-                            ),
-                            new Column(
 
-                              children: <Widget>[
-                                RaisedButton(
-                                child: Text("sign up"),
-                                 onPressed: sendToDatabase,
-                                  color: Colors.red,
-                                  textColor: Colors.white,
-                                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                  splashColor: Colors.grey,
-                                ),
-                                ]
-                            ),
 
-                            
-                          ],
-                    )
-                      )
-                      )                 
-                       ]
-                ),
-              ],
-                    )
-            );
-}
-}
-void sendToDatabase(){
-
-}
